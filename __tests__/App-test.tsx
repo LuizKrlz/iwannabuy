0b3f40e5db9ed62cb01a32f205833f8a3ba2128f
Luiz Carlos Rocha Correa <luiz.correa.dev@gmail.com>
2024-08-24 13:15:03 +0100

    feat: add config for tests

---
+++ __tests__/App-test.tsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react-native";
+import App from "../App";
+
+describe("<App />", () => {
+  test("Text renders correctly on HomeScreen", () => {
+    const { getByText } = render(<App />);
+
+    getByText("Hello");
+  });
+});
