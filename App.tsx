9b7d19b3d899725ccc570869d309373d3b7110df
Luiz Carlos Rocha Correa <luiz.correa.dev@gmail.com>
2024-08-25 21:55:11 +0100

    feat: config wrapper for app main

--- App.tsx
+++ App.tsx
@@ -1,10 +1,31 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
+import { ThemeProvider, useTheme } from "@shopify/restyle";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import theme, { Theme } from "./src/styles/theme";
 import { Routes } from "./src/routes";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchIntervalInBackground: false,
+    },
+  },
+});
+
 export default function App() {
+  const myTheme = useTheme<Theme>();
+  const defaultTheme = DefaultTheme;
+
+  defaultTheme.colors.background = myTheme.colors["bg"];
+
   return (
-    <NavigationContainer>
-      <Routes />
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <NavigationContainer theme={defaultTheme}>
+          <Routes />
+        </NavigationContainer>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
