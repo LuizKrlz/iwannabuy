80cf36a22a4cf478831183312f442efae2eb0f8e
Luiz Carlos Rocha Correa <luiz.correa.dev@gmail.com>
2024-08-26 08:31:31 +0100

    feat: add new route Cart List

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { ListProductsScreen } from "../screens/ListProductsScreen";
 import { TRootStackParamList } from "./types";
+import { CartScreen } from "../screens/CartScreen";
 
 const Stack = createNativeStackNavigator<TRootStackParamList>();
 
@@ -8,6 +9,7 @@ export function Routes() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="products" component={ListProductsScreen} />
+      <Stack.Screen name="cart" component={CartScreen} />
     </Stack.Navigator>
   );
 }
