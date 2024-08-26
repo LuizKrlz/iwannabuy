969baf3c361362234b6d4981ada2c9bdf451b7f1
Luiz Carlos Rocha Correa <luiz.correa.dev@gmail.com>
2024-08-26 23:39:57 +0100

    fix: types of list screen products

--- src/screens/ListProductsScreen/types.ts
+++ src/screens/ListProductsScreen/types.ts
@@ -1,7 +1,7 @@
 import { NativeStackScreenProps } from "react-native-screens/lib/typescript/native-stack/types";
 import { TRootStackParamList } from "../../routes/types";
 
-export type TListProductsScreen = Pick<
-  NativeStackScreenProps<TRootStackParamList, "products">,
-  "navigation"
+export type TListProductsScreen = NativeStackScreenProps<
+  TRootStackParamList,
+  "products"
 >;
