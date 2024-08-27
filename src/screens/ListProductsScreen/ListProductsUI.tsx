20558e9f57a1a4cfa283e63f7c7b50db6e150dc0
Luiz Carlos Rocha Correa <luiz.correa.dev@gmail.com>
2024-08-27 09:25:23 +0100

    fix: add callback and improve list

--- src/screens/ListProductsScreen/ListProductsUI.tsx
+++ src/screens/ListProductsScreen/ListProductsUI.tsx
@@ -1,4 +1,4 @@
-import { FlatList } from "react-native";
+import { ActivityIndicator, FlatList } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useTheme } from "@shopify/restyle";
 import { Theme } from "../../styles/theme";
@@ -6,6 +6,8 @@ import { Header } from "../../components/organisms/Header";
 import { CardProduct } from "../../components/organisms/CardProduct";
 import { useListProductsScreen } from "./useListProducts";
 import { EmptyListProducts } from "../../components/molecules/EmptyListProducts";
+import { useCallback } from "react";
+import { TProduct } from "../../integration/products/types";
 
 export function ListProductsUI({
   isScrolling,
@@ -13,17 +15,38 @@ export function ListProductsUI({
   handleGoToCart,
   handlePressInProduct,
   itemSelected,
+  isFetching,
   list,
   listenerOnScroll,
   showAnimationCart,
+  fetchNextPage,
 }: ReturnType<typeof useListProductsScreen>) {
   const insets = useSafeAreaInsets();
   const theme = useTheme<Theme>();
 
+  const renderItem = useCallback(
+    ({ item }: { item: TProduct }) => {
+      const findInCart = existsInCart(item);
+
+      return (
+        <CardProduct
+          product={item}
+          isDisabled={showAnimationCart && itemSelected === item.id}
+          alreadyAdded={!!findInCart}
+          handlePressButton={() => handlePressInProduct(!!findInCart, item)}
+        />
+      );
+    },
+    [existsInCart, handlePressInProduct, showAnimationCart, itemSelected]
+  );
+
   return (
     <FlatList
       numColumns={2}
+      extraData={isFetching}
+      initialNumToRender={3}
       stickyHeaderIndices={[0]}
+      columnWrapperStyle={{ gap: theme.spacing.m, marginVertical: 5 }}
       showsVerticalScrollIndicator={false}
       contentContainerStyle={{
         paddingBottom: insets.bottom + theme.spacing.m,
@@ -31,23 +54,16 @@ export function ListProductsUI({
       }}
       keyExtractor={(item) => item.id.toString()}
       data={list}
+      refreshing={isFetching}
       ListHeaderComponent={() => (
         <Header handleGoToCart={handleGoToCart} isScrolling={isScrolling} />
       )}
+      ListFooterComponent={isFetching ? <ActivityIndicator /> : <></>}
+      onEndReachedThreshold={3}
+      onEndReached={() => fetchNextPage()}
       ListEmptyComponent={() => <EmptyListProducts />}
       onScroll={listenerOnScroll}
-      renderItem={({ item }) => {
-        const findInCart = existsInCart(item);
-
-        return (
-          <CardProduct
-            product={item}
-            isDisabled={showAnimationCart && itemSelected === item.id}
-            alreadyAdded={!!findInCart}
-            handlePressButton={() => handlePressInProduct(!!findInCart, item)}
-          />
-        );
-      }}
+      renderItem={renderItem}
     />
   );
 }
