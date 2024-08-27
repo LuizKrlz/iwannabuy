c2a496547b8d5600fbee2264c6d8416441e96034
Luiz Carlos Rocha Correa <luiz.correa.dev@gmail.com>
2024-08-27 09:24:19 +0100

    feat: add memo for component

--- src/components/organisms/CardProduct.tsx
+++ src/components/organisms/CardProduct.tsx
@@ -4,6 +4,7 @@ import { currencyFormat } from "../../utils/numbersFormat";
 import { TProduct } from "../../integration/products/types";
 import { CardProductButton } from "../molecules/CardProductButton/CardProductButton";
 import { ImageWithFallback } from "../atoms/ImageWithFallback";
+import { memo } from "react";
 
 type TCardProduct = {
   alreadyAdded?: boolean;
@@ -12,43 +13,45 @@ type TCardProduct = {
   isDisabled?: boolean;
 };
 
-export function CardProduct({
-  alreadyAdded,
-  handlePressButton,
-  product,
-  isDisabled,
-}: TCardProduct) {
-  return (
-    <Box
-      flex={1}
-      margin="s"
-      width={188}
-      bg="card"
-      borderRadius={20}
-      overflow="hidden"
-    >
-      {product.images && product.images.length && (
-        <ImageWithFallback uri={product.images[0]} height={180} />
-      )}
-      <Box p="s" flex={1} maxHeight={100} justifyContent="space-between">
-        <Typography
-          numberOfLines={2}
-          variant="titleThree"
-          fontWeight={"500"}
-          color="black"
-        >
-          {product.title}
-        </Typography>
+export const CardProduct = memo(
+  ({ alreadyAdded, handlePressButton, product, isDisabled }: TCardProduct) => {
+    return (
+      <Box
+        flex={1}
+        // margin="s"
+        width={188}
+        bg="card"
+        borderRadius={20}
+        overflow="hidden"
+      >
+        {product.images && product.images.length && (
+          <ImageWithFallback uri={product.images[0]} height={180} />
+        )}
+        <Box p="s" flex={1} maxHeight={100} justifyContent="space-between">
+          <Typography
+            numberOfLines={2}
+            variant="titleThree"
+            fontWeight={"500"}
+            color="black"
+          >
+            {product.title}
+          </Typography>
 
-        <Typography variant="body" fontWeight={"bold"} textAlign="right" mt="s">
-          {currencyFormat(product.price)}
-        </Typography>
+          <Typography
+            variant="body"
+            fontWeight={"bold"}
+            textAlign="right"
+            mt="s"
+          >
+            {currencyFormat(product.price)}
+          </Typography>
+        </Box>
+        <CardProductButton
+          isDisabled={isDisabled}
+          onPress={handlePressButton}
+          alreadyAdded={alreadyAdded}
+        />
       </Box>
-      <CardProductButton
-        isDisabled={isDisabled}
-        onPress={handlePressButton}
-        alreadyAdded={alreadyAdded}
-      />
-    </Box>
-  );
-}
+    );
+  }
+);
